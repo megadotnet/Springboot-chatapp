34bcae74912addc4926560bba08b180d06270adc
megadotnet <wintersunair@gmail.com>
2024-09-09 16:00:13 +0800

    fixed:default page debugging issues

--- chatroon-backend/src/main/resources/static/app.js
+++ chatroon-backend/src/main/resources/static/app.js
@@ -14,51 +14,16 @@ function setConnected(connected) {
 
 function connect() {
 
-
-
-		$.ajax({
-			url : 'ws/getSubject',
-			dataType : 'json',
-			headers : {
-				"X-Requested-With" : "XMLHttpRequest"
-			},
-			success : function(data) {
-			     console.log(data)
-
 				    var socket = new SockJS('/ws');
                     stompClient = Stomp.over(socket);
                     //认证
-                    stompClient.connect({"X-Authorization": "Bearer " + data.accessToken}, function (frame) {
+                    stompClient.connect({"X-Authorization": "Bearer " + "xx"}, function (frame) {
                         setConnected(true);
                         console.log('Connected: ' + frame);
                         stompClient.subscribe('/chatroom/public', function (greeting) {
                             showGreeting(JSON.parse(greeting.body).message);
                         });
                     });
-			},
-			error : function(response) {
-				if (response.status == 401 || response.status == 402) {
-					var loginUrl = response.getResponseHeader("loginurl");
-					if (loginUrl) {
-						var backUrl = window.location.hash;
-						var localBaseIndexOf = loginUrl
-								.indexOf("redirect_uri=");
-						if (localBaseIndexOf != -1) {
-							var localBase = loginUrl.substring(
-									localBaseIndexOf + 13, loginUrl
-											.indexOf("oauth2-login"));
-							backUrl = window.location.href.replace(localBase,
-									"");
-						}
-						//跳转到系统登录页面
-						window.location.href = loginUrl + "?backUrl="
-								+ encodeURIComponent(backUrl);
-					} else {
-						window.location.reload(true);
-					}
-				}
-			}
-		});
 
 
 }
@@ -94,38 +59,5 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
 
-
-    		$.ajax({
-    			url : 'ws/getSubject',
-    			dataType : 'json',
-    			headers : {
-    				"X-Requested-With" : "XMLHttpRequest"
-    			},
-    			success : function(data) {
-    			     console.log(data)
-    			},
-    			error : function(response) {
-    				if (response.status == 401 || response.status == 402) {
-    					var loginUrl = response.getResponseHeader("loginurl");
-    					if (loginUrl) {
-    						var backUrl = window.location.hash;
-    						var localBaseIndexOf = loginUrl
-    								.indexOf("redirect_uri=");
-    						if (localBaseIndexOf != -1) {
-    							var localBase = loginUrl.substring(
-    									localBaseIndexOf + 13, loginUrl
-    											.indexOf("oauth2-login"));
-    							backUrl = window.location.href.replace(localBase,
-    									"");
-    						}
-    						//跳转到系统登录页面
-    						window.location.href = loginUrl + "?backUrl="
-    								+ encodeURIComponent(backUrl);
-    					} else {
-    						window.location.reload(true);
-    					}
-    				}
-    			}
-    		});
 });
 
